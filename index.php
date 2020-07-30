452331399fc869f1d8857c3f811831bbd1ffdb61
ikkoheroes <68898293+ikkoheroes@users.noreply.github.com>
2020-07-30 18:40:08 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,156 @@
+<!--参考サイト
+http://hanabishi-f.co.jp/?gclid=Cj0KCQiApaXxBRDNARIsAGFdaB9cWlMEUkp_TUJtq0WhBjDc03np0vcI8bn087DO5ihkg0jD0kBEIpkaAtvVEALw_wcB-->
+
+<!DOCTYPE html>
+<html lang="jp" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>デモcontact</title>
+  <link rel="stylesheet" href="urbantrend_contact_0721.css">
+  <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+</head>
+
+<body>
+  <header>
+    <div class="header">
+      <div class="header-wrapper">
+        <div class="header-icon">
+          <img src="https://f.easyuploader.app/eu-prd/upload/20200723173150_776a39376e344c426b73.png" alt="">
+        </div>
+
+
+        <div class="header-menu">
+          <ul>
+            <li><a href="urbantrend.html">トップ</a></li>
+            <li><a href="C:/Users/user_2/Desktop/lesson/urbantrend/gaiyou/urbantrend_gaiyou.html">アーバントレンドについて</a></li>
+            <li><a href="C:/Users/user_2/Desktop/lesson/urbantrend/contact/urbantrend_contact.html">お問い合わせ</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </header>
+
+
+
+
+  <div class="main-wrapper">
+    <div class="main-img">
+
+      <img src="https://cdn.pixabay.com/photo/2013/11/25/09/47/japan-217878_960_720.jpg" alt="">
+
+    </div>
+  </div>
+
+  //以上、固定ヘッダー
+
+  <div class="wrapper-title">
+    <p>お問い合わせ</p>
+  </div>
+
+
+  <div class="sub-wrapper">
+    <div class="contact-text">
+      <p>ご依頼・ご相談などは下記フォームよりお気軽にお問い合わせください。<br>
+        また、お電話でのお問い合わせは下記の番号より承っております。<br>
+        TEL：03-3965-7603（10：00 - 20：00 水曜定休）</p>
+      </div>
+
+
+      <div class="mail-form">
+
+        <form method="post"　class="" action="check/urbantrend_contactcheck_0721.php">
+
+          <table class="form">
+            <tbody>
+              <tr>
+                <th>件名</th>
+                <td>
+                  <label>
+                    <input type="radio" name="subject" value="物件売買希望" checked="checked">
+                    物件売買希望
+
+                    <input type="radio" name="subject" value="リフォーム希望">
+                    リフォーム希望
+
+                    <input type="radio" name="subject" value="その他">
+                    その他
+
+                  </label>
+                </td>
+              </tr>
+
+              <tr>
+                <th>お名前</th>
+                <td>
+                  <input type="text" name="nameInput" id="name" ></td>
+                </tr>
+
+                <tr>
+                  <th>会社名</th>
+                  <td>
+                    <input type="text" name="compInput" id="company" ></td>
+                  </tr>
+
+                  <tr>
+                    <th>お電話番号</th>
+                    <td>
+                      <input type="tel" name="telInput" id="telephone" ></td>
+                    </tr>
+
+                    <tr>
+                      <th>メールアドレス</th>
+                      <td>
+                        <input type="email" name="mailInput" id="mail" ></td>
+                      </tr>
+
+                      <tr>
+                        <th>問い合わせ内容</th>
+                        <td>
+                          <textarea name="contentsInput" cols="40" rows="10"  id="contents" ></textarea></td>
+                        </tr>
+                      </tbody>
+                    </table>
+                    <div class="check-button">
+
+
+                    <input type="submit" value="内容を確認します" class="button">
+
+                    </div>
+                  </form>
+
+
+                </div>
+
+              </div>
+            </div>
+  </div>
+
+
+
+
+
+
+
+            以下、固定フッター
+
+            <div class="footer-img">
+              <img src="https://images.unsplash.com/photo-1526404423292-15db8c2334e5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=375&q=80" alt="">
+            </div>
+
+            <div class="contact-here">
+              <span class="fas fa-paper-plane mail"></span>
+              <div class="contact-txt">
+                ご依頼やご相談など、弊社へのお問い合わせはこちらから。<br>
+                TEL：03-3965-7603（10：00 - 20：00 水曜定休）
+              </div>
+              <a href="#" class="btn-square-shadow">お問い合わせフォーム</a>
+            </div>
+
+            <footer>
+              <div class="footer">
+                <h6>Copyright 2017 Urban-trend Inc.All rights reserved</h6>
+
+              </div>
+            </footer>
+          </body>
+          </html>
